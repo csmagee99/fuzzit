bf9cc9158532a8ec60ed078a334db6d60f6ece31
csmagee99 <csmagee89@gmail.com>
2020-08-28 21:50:21 -0500

    Update fuzzit.py

--- fuzzit.py
+++ fuzzit.py
@@ -3,11 +3,14 @@ import requests
 target_url = input("Enter target url to fuzz: ")
 
 with open("fuzzer_list.txt", "r") as fuzzer_list:
-  for word in fuzzer_list:
-    resp = requests.post(target_url + word)
-    print(target_url + word)
-    print(resp)
-    if resp.ok:
-        print('OK!')
-    else:
-        print('Boo!')
+    try:
+        for word in fuzzer_list:
+            resp = requests.post(target_url + word)
+            print(target_url + word)
+            print(resp)
+            if resp.ok:
+                print('Fuzz yeah!!')
+            else:
+                print('Boo!')
+    except:
+        pass
